303f12ddee3104d62ee2e7d2b72b4bbc8817a395
lipeng <85272827+roc80@users.noreply.github.com>
2025-06-09 01:07:48 +0800

    从Hexo迁移文章，更改schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,13 +6,16 @@ const blog = defineCollection({
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+        title: z.string(),
+        description: z.string().optional(),
+        // 直接期望一个 Date 类型
+        // Astro 的内容集合系统会负责将你的日期字符串解析为 Date 对象
+        pubDate: z.date(),
+        updatedDate: z.date().optional(),
+        tags: z.array(z.string()).optional(),
+        categories: z.array(z.string()).optional(),
+        heroImage: image().optional(),
+    }),
 });
 
 export const collections = { blog };
