e19245aac28e0b3fa3859db81d770f0e88a4fdc3
lipeng <85272827+roc80@users.noreply.github.com>
2025-06-09 21:55:19 +0800

    dev 更新个人信息，将周记拆分出去

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,10 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
+    loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string().optional(),
-        // 直接期望一个 Date 类型
-        // Astro 的内容集合系统会负责将你的日期字符串解析为 Date 对象
         pubDate: z.date(),
         updatedDate: z.date().optional(),
         tags: z.array(z.string()).optional(),
@@ -18,4 +14,31 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const weeks = defineCollection({
+    loader: glob({ base: './src/content/weeks', pattern: '**/*.{md,mdx}' }),
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        pubDate: z.date(),
+        updatedDate: z.date().optional(),
+        tags: z.array(z.string()).optional(),
+        categories: z.array(z.string()).optional(),
+        heroImage: image().optional(),
+    }),
+});
+
+const about = defineCollection({
+    loader: glob({ base: './src/content/about', pattern: '**/*.{md,mdx}' }),
+});
+
+const home = defineCollection({
+    loader: glob({ base: './src/content/home', pattern: '**/*.{md,mdx}' }),
+});
+
+
+export const collections = {
+    blog,
+    weeks,
+    about,
+    home,
+};
---
+++ src/content/home/index.md
@@ -0,0 +1,31 @@
+## 👋 Hello, Here is Roc's Space
+
+欢迎来到我的个人网站，这里记录了我的学习、开发和生活经历。
+
+---
+
+#### 🤖 技术栈
+
+- **后端 & 数据库**：Java / Spring Boot / MySQL / Redis（了解）
+- **前端**：HTML / CSS / TypeScript / Astro(本站基于Astro构建) / Vue / Antd
+
+---
+
+#### 📂 我的内容
+
+- 🪐 [博客](/blog)
+- 🛠️ [项目](https://github.com/roc80)
+- 👤 [关于我](/about)
+- 📡 [联系我](mailto:lipeng_8080@163.com)
+
+---
+
+#### 🚀 近期动态
+
+- ✅ 完成了本站的搭建并部署上线
+- ⏳ 找伙伴（移动端H5 APP）系统 *developing*
+
+---
+
+> 🌟 感谢你的访问，如果你也热爱开发或内容创作，欢迎联系我或一起交流！
+
