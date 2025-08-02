eecc6aa74dcf9667eeed4bc695e668b550d941c4
lipeng <85272827+roc80@users.noreply.github.com>
2025-08-02 19:45:05 +0800

    部分文章添加评论功能

--- new-post-blog.sh
+++ new-post-blog.sh
@@ -21,6 +21,24 @@ pubDate: ${CURRENT_DATE}
 tags: []
 ---
 
+
+<script src="https://giscus.app/client.js"
+        data-repo="roc80/Blog"
+        data-repo-id="R_kgDOO4NnfQ"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOO4Nnfc4Ctshe"
+        data-mapping="pathname"
+        data-strict="1"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="preferred_color_scheme"
+        data-lang="zh-CN"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async>
+</script>
+
 EOF
 
 echo "文章 '${FILENAME}.md' 已创建在 ${POST_DIR}."
